e2aae091c433e8c009b02606efdc3c04a77d3770
ppaycc <ppaycc@gmail.com>
2024-02-08 15:48:45 +0200

    env

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 import { createServer } from 'node:http';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = createServer(app);
 const io = new Server(server);
@@ -17,9 +17,10 @@ io.on('connection', (socket) => {
 })
 
 app.get('/', (req, res) => {
+    console.log('Hello World!')
     res.send('Welcome to my server!');
 });
 
-server.listen(port, () => {
+server.listen(port, '0.0.0.0',() => {
     console.log(`Server is running on port ${port}`);
 });
